32ed0e91e99fc7f35e228bbe99c2392769f73d6e
Matthias Ellerbeck <file_not_found@mailbox.org>
2021-04-06 13:46:57 +0200

    Attemped to multithread the enigne (failed for now)

--- StateDiagramEngine/Assets/StatechartEngine/StatechartEngine.cs
+++ StateDiagramEngine/Assets/StatechartEngine/StatechartEngine.cs
@@ -1,15 +1,46 @@
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using System.Threading;
 
 public class StatechartEngine : MonoBehaviour
 {
+    const int batchSize = 32;
+    const int threadCount = 32;
+
+    class InstanceList
+    {
+        public List<StatechartInstance> instances;
+        volatile int position;
+
+        public int Next()
+        {
+            lock (instances)
+            {
+                position += batchSize;
+                return position;
+            }
+        }
+
+
+        public void Reset()
+        {
+            position = 0;
+        }
+    }
+
     static StatechartEngine instance = null;
     static bool dead = false;
 
-    readonly IList<StatechartInstance> updateInstances = new List<StatechartInstance>();
-    readonly IList<StatechartInstance> lateInstances = new List<StatechartInstance>();
-    readonly IList<StatechartInstance> fixedInstances = new List<StatechartInstance>();
+    readonly List<Thread> threads;
+    readonly EventWaitHandle monitor_main;
+    readonly EventWaitHandle monitor_workers;
+    volatile int available = 0;
+    volatile InstanceList currentTask;
+
+    readonly InstanceList updateInstances;
+    readonly InstanceList lateInstances;
+    readonly InstanceList fixedInstances;
 
 #if SC_PROFILE_UPDATE
     System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
@@ -32,19 +63,44 @@ public class StatechartEngine : MonoBehaviour
         return instance;
     }
 
-    
+
+    StatechartEngine()
+    {
+        monitor_workers = new EventWaitHandle(false, EventResetMode.ManualReset);
+        monitor_main = new EventWaitHandle(true, EventResetMode.ManualReset);
+
+        threads = new List<Thread>(threadCount);
+        for (int i = 0; i < threadCount; i++)
+        {
+            threads.Add(new Thread(ExecuteInstances)
+            { Name = "SCthread " + i,
+              IsBackground = true });
+
+            threads[threads.Count - 1].Start();
+        }
+        available = threads.Count;
+
+        updateInstances = new InstanceList()
+        { instances = new List<StatechartInstance>() };
+        lateInstances = new InstanceList()
+        { instances = new List<StatechartInstance>() };
+        fixedInstances = new InstanceList()
+        { instances = new List<StatechartInstance>() };
+    }
+
+
     public static void AddInstance(StatechartInstance ins)
     {
         switch(ins.GetMode())
         {
             case Statechart.Mode.Unity_Update:
-                GetInstance()?.updateInstances.Add(ins);
+                GetInstance()?.updateInstances.instances.Add(ins);
                 break;
             case Statechart.Mode.Unity_Late_Update:
-                GetInstance()?.lateInstances.Add(ins);
+                GetInstance()?.lateInstances.instances.Add(ins);
                 break;
             case Statechart.Mode.Unity_Fixed_Update:
-                GetInstance()?.fixedInstances.Add(ins);
+                GetInstance()?.fixedInstances.instances.Add(ins);
                 break;
         }
     }
@@ -55,13 +111,13 @@ public class StatechartEngine : MonoBehaviour
         switch (ins.GetMode())
         {
             case Statechart.Mode.Unity_Update:
-                GetInstance()?.updateInstances.Remove(ins);
+                GetInstance()?.updateInstances.instances.Remove(ins);
                 break;
             case Statechart.Mode.Unity_Late_Update:
-                GetInstance()?.lateInstances.Remove(ins);
+                GetInstance()?.lateInstances.instances.Remove(ins);
                 break;
             case Statechart.Mode.Unity_Fixed_Update:
-                GetInstance()?.fixedInstances.Remove(ins);
+                GetInstance()?.fixedInstances.instances.Remove(ins);
                 break;
         }
     }
@@ -73,35 +129,103 @@ public class StatechartEngine : MonoBehaviour
         stopwatch.Start();
 #endif
 
-        foreach (StatechartInstance i in updateInstances)
-            i.SuperStep();
+        DoWork(updateInstances);
 
 #if SC_PROFILE_UPDATE
         stopwatch.Stop();
         accTime.Accumulate(stopwatch);
         stopwatch.Reset();
         if (accTime.SampleCount % 100 == 0)
-            Debug.Log("Executing " + updateInstances.Count + " instances.\n" + accTime.GetStatistics(AccumulatedTime.TimeUnit.µs));
+            Debug.Log("Executing " + updateInstances.instances.Count + " instances.\n" + accTime.GetStatistics(AccumulatedTime.TimeUnit.µs));
 #endif
     }
 
 
     void LateUpdate()
     {
-        foreach (StatechartInstance i in lateInstances)
-            i.SuperStep();
+        DoWork(lateInstances);
     }
 
 
     void FixedUpdate()
     {
-        foreach (StatechartInstance i in fixedInstances)
-            i.SuperStep();
+        DoWork(fixedInstances);
     }
 
 
     private void OnApplicationQuit()
     {
         dead = true;
+    }
+
+
+    void DoWork(InstanceList task)
+    {
+        if (task.instances.Count <= 0)
+            return;
+
+        if (threadCount <= 0)
+        {
+            foreach (var i in task.instances)
+                i.SuperStep();
+            return;
+        }
+
+        currentTask = task;
+        monitor_workers.Set();
+        // Wait until all threads have started
+        if (!monitor_main.WaitOne(34))
+            Debug.LogError("One or more threads did not start.");
+        monitor_main.Reset();
+        monitor_workers.Reset();
+        // Wait until all threads have finished
+        if (!monitor_main.WaitOne(1000))
+            Debug.LogError("Deadlock or loop detected.");
+        monitor_main.Reset();
+
+        task.Reset();
+
+        if (available != threadCount)
+            Debug.Log(available);
+    }
+
+
+    [MethodImpl(MethodImplOptions.Synchronized)]
+    void ThreadStarted()
+    {
+        available--;
+        if (available <= 0)
+            monitor_main.Set();
+    }
+
+
+    [MethodImpl(MethodImplOptions.Synchronized)]
+    void ThreadFinished()
+    {
+        available++;
+        if (available >= threads.Count)
+            monitor_main.Set();
+    }
+
+
+    void ExecuteInstances()
+    {
+        while (!dead)
+        {
+            monitor_workers.WaitOne();
+            GetInstance().ThreadStarted();
+
+            // While work is available
+            int batch = currentTask.Next();
+            while (batch < currentTask.instances.Count)
+            {
+                // A single batch
+                for (int i = batch; i < batch + batchSize && i < currentTask.instances.Count; i++)
+                    currentTask.instances[i].SuperStep();
+                batch = currentTask.Next();
+            }
+
+            GetInstance().ThreadFinished();
+        }
     }
 }
